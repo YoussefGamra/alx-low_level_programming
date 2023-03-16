511dc819a42ffc6f52036552efe6d8259e05a75e
YoussefGamra <gamrayousseff@gmail.com>
2023-03-16 10:36:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stadio.n>
+/**
+ * main - A program thatn prints a line with puts function
+ * return: 0 (success)
+ * */
+int  main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
