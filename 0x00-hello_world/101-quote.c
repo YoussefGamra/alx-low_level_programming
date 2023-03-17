2a5ace3550d0f0d29a433c7ef7ae355025737d48
YoussefGamra <gamrayousseff@gmail.com>
2023-03-16 18:23:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main -A c prograù that prints a line to the standard error
+ * main - Prints a quote to the standard error
  *
- * Return: 1 (not succes)
-*/
-int  main(void)
+ * Return: Always 1
+ */
+int main(void)
 {
-        fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n" ,stdout);
-        return (1);
+    write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
