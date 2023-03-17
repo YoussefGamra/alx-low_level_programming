84e4678f46a961d2b344e26698ef3183ec6e89a1
YoussefGamra <gamrayousseff@gmail.com>
2023-03-16 17:05:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main -Enty point
- *  Return: always 0 (Success)
+ * main - this is the  main function
+ *
+ *  Return: 0 when  Successfull
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
