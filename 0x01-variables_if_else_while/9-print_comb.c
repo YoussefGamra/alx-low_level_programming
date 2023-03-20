88a5669548c98bf8288a9782b7d240b3c70e7153
YoussefGamra <gamrayousseff@gmail.com>
2023-03-20 13:14:08 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: 'print comb of numbers'
+ * Return: Always
+ */
+int main(void)
+{
+	int n;
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n = 48; n <= 57; n++)
+			putchar(n);
+			if (n == 57; n++)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+	}
+			putchar('\n');
+
+	return (0);
+}
