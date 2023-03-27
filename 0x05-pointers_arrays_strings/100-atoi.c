625959fab1a8e1489286f87e3766d9dfe71c6f52
YoussefGamra <gamrayousseff@gmail.com>
2023-03-27 16:25:34 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,30 @@
-#include 
-/**
-* _atoi - function that convert a string to an integer.
-* @s: string
-* Return: integer
-*/
+#include "main.h"
+#include <stdio.h>
 
-int _atoi(char *s)
+/**
+ * main - check the code for main School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	return (*s);
+    int nb;
+
+    nb = _atoi("98");
+    printf("%d\n", nb);
+    nb = _atoi("-402");
+    printf("%d\n", nb);
+    nb = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", nb);
+    nb = _atoi("214748364");
+    printf("%d\n", nb);
+    nb = _atoi("0");
+    printf("%d\n", nb);
+    nb = _atoi("Suite 402");
+    printf("%d\n", nb);
+    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", nb);
+    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", nb);
+    return (0);
 }
