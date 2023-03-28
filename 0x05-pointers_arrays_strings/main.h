72ef014dfac061ffca49917bf8deef9733bdec94
YoussefGamra <gamrayousseff@gmail.com>
2023-03-28 10:25:04 -0700

    task2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
-#define MAIN_h
+#define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -17,4 +16,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif
+#endif /* MAIN_H */
