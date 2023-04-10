b2418d98b47f84766473d4a85507bbf386fb0a22
YoussefGamra <gamrayousseff@gmail.com>
2023-04-10 11:01:26 -0700

    0x0A-argc_av

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,39 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-  * main - Program that prints the result  that multiplies two numbers.
-  * @argc: count
-  * @argv: array
-  *
-  * Return: Always 0
-  **/
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-	(void) argc;
-	if (*(argv + 1) == '\0' || *(argv + 2) == '\0')
+	int i;
+	int j;
+	int k;
+	int mult = 0;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+		return (0);
 	}
-	else
-		printf("%d\n", (atoi(*(argv + 1)) * atoi(*(argv + 2))));
+	else if (argc == 3)
+	{
+		for (i = 0 ; i < argc ; i++)
+		{
+			if (i == 1)
+			{
+				j = atoi(argv[i]);
+			}
+			else if (i == 2)
+			{
+				k = atoi(argv[i]);
+			}
+		}
+	mult = j * k;
+	printf("%d\n", mult);
+	}
 	return (0);
 }
-
