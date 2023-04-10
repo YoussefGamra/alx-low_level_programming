4bf88c7c71852f3e1f10ae44994458f02997a68d
YoussefGamra <gamrayousseff@gmail.com>
2023-04-10 10:58:12 -0700

    0x0A-argc_av

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,17 @@
-#include ".main.h"
 #include <stdio.h>
+
 /**
-  * main - Program that prints its name
-  * @argc: count
-  * @argv: array
-  *
-  * Return: Always 0
-  **/
-int main(int argc, char **argv)
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	for (i = 0; i < argc ; i++)
 	{
-		printf("%s", argv[i]);
-		printf("\n");
+		printf("%s\n", argv[i]);
 	}
 	return (0);
-}
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,13 @@
-#include "main.h"
 #include <stdio.h>
-/**
-  * main - Program that prints the number of arguments passed into it.
-  * @argc: count
-  * @argv: array
-  *
-  * Return: Always 0
-  **/
-int main(int argc, char *argv[])
-{
-	(void) argv;
 
+/**
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
 	printf("%d\n", argc - 1);
 	return (0);
 }
