488e2670c93191fd08164c55499328654e02af18
YoussefGamra <gamrayousseff@gmail.com>
2023-05-12 17:37:24 -0700

    task0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
-#ifndef FILE_IO
-#define FILE_IO
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int _putchar(char c);
@@ -11,4 +11,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* main_H */
